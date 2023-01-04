11b3a81563cc2d50fedcc0582e31bb5d71f0deb2
sameera sood <sameerasood@gmail.com>
2023-01-04 21:09:54 +0000

    Save the list of students in csv file

--- directory.rb
+++ directory.rb
@@ -18,6 +18,7 @@ def print_menu
 #1. print the menu and ask the user what it wants to do
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -27,6 +28,18 @@ def show_students
   print_footer
 end
 
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+    #iterate over the array of students
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join (",")
+      file.puts csv_line
+    end
+file.close
+end
+
 def process(selection)
   #3. do what the user has asked
   case selection
@@ -34,6 +47,8 @@ def process(selection)
       students = input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit #this will cause the program to terminate
     else
