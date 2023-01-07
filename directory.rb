ccdd625cbfe75c07bb45a861cf405508198b235e
sameera sood <sameerasood@gmail.com>
2023-01-07 19:46:31 +0000

    Student Directory Week 4

--- directory.rb
+++ directory.rb
@@ -1,43 +1,18 @@
-# we put the srudent names in a nested array, along with the month of their cohort as a symbol.
-# students = [
-# {name: "Dr. Hannibal Lecter",cohort: :november},
-# {name: "Darth Vadar", cohort: :november},
-# {name: "Nurse Ratched", cohort: :november},
-# {name: "Michael Corleone", cohort: :november},
-# {name: "Alex DeLarge",cohort: :november},
-# {name: "The Wicked Witch of the West",cohort: :november},
-# {name: "Terminator",cohort: :november},
-# {name: "Freddy Krueger",cohort: :november},
-# {name: "The Joker",cohort: :november},
-# {name: "Joffrey Baratheon",cohort: :november},
-# {name: "Norman Bates",cohort: :november}
-# ]
-
 @students = []
 def print_menu
 #1. print the menu and ask the user what it wants to do
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
+  puts "3. Save the student list"
+  puts "4. Load the student list"
   puts "9. Exit"
 end
 
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
-def save_students
-  #open the file for writing
-  file = File.open("students.csv", "w")
-    #iterate over the array of students
-    @students.each do |student|
-      student_data = [student[:name], student[:cohort]]
-      csv_line = student_data.join (",")
-      file.puts csv_line
-    end
-file.close
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
 end
 
 def process(selection)
@@ -49,29 +24,45 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
+      puts "Goodbye!"
       exit #this will cause the program to terminate
     else
       puts "I don't know what you meant, please try again."
   end
 end
 
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
-
 def input_students
   puts "Enter the name of student"
   puts "To exit, hit return twice"
-  name = gets.chomp
+  add_name
+end
+
+def add_name
+    name = STDIN.gets.chomp
     while !name.empty? do
-    @students << {name: name, cohort: :november}
+    add_students(name, :november)
     puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
     end
+end
+
+def add_students(name, cohort)
+@students << {name: name, cohort: cohort}
+end
+
+def load_students
+  puts "Enter filename"
+  filename = gets.chomp
+  try_load_students(filename)
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def print_header
@@ -89,7 +80,52 @@ end
 
 #Here we print the total number of students in the academy
 def print_footer
-  print "Overall, we have #{@students.count} great students"
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  #open the file for writing
+  puts "Enter filename to save"
+  filename = gets.chomp
+    if File.exist?(filename) #check if the filename exists
+    save_file(filename)
+  else
+    filename = "students.csv"
+    save_file(filename)
+  end
+end
+
+def save_file(filename)
+  file = File.open("students.csv", "w")
+    #iterate over the array of students
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join (",")
+      file.puts csv_line
+    end
+    file.close
+  puts "#{@students.count} students saved to the file."
+  end
+
+
+def try_load_students(filename = "students.csv")
+  #filename = ARGV.first
+  if File.exist?(filename) #check if the filename exists
+    read_file(filename)
+  else
+    filename = "students.csv"
+    read_file(filename)
+  end
+end
+
+def read_file(filename)
+file = File.open(filename, "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+     add_students(name, cohort.to_sym)
+  end
+  file.close
+  puts "#{@students.count} students loaded from the #{filename}"
 end
 
 interactive_menu
